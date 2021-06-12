45b8da96a0a0d8516052fda817630c428e147043
Ruben Cristea <rubendcristea@gmail.com>
2021-06-12 16:08:32 -0400

    Added draggable capability to one of the folders

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,16 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import './Home.css'
 import { MenuBar } from '../menu-bar/MenuBar'
 import { Dock } from '../dock/Dock'
+import Draggable from 'react-draggable'
 import background from '../../assets/mac_bigsur.jpeg'
+import folder from '../../assets/mac_bigsur_folder.png'
 
 export class Home extends Component {
+  constructor(props) {
+    super(props)
+
+    this.nodeRef = createRef()
+
+    this.rareEPRef = createRef()
+    this.newContentRef = createRef()
+    this.newSamplesRef = createRef()
+  }
+
+  componentDidMount() {
+    this.rareEPRef.current.setAttribute('draggable', false)
+  }
+
   render() {
     return (
       <div>
         <MenuBar />
         <div className='home-container'>
           <div className='home-screen' style={{backgroundImage:`url(${background})`}}></div>
+          <Draggable bounds='parent' nodeRef={this.nodeRef}>
+            <div className='drag-box' ref={this.nodeRef}>
+              <div className='folder-icon'>
+                <img src={folder} alt='MacOS BigSur Folder Icon' ref={this.rareEPRef}/>
+              </div>
+              <p className='folder-name'>RARE EP</p>
+            </div>
+          </Draggable>
         </div>
         <Dock />
       </div>
