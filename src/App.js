b1bde280cffee8ab7c384226b50e3d662b09e5eb
Ruben Cristea <rubendcristea@gmail.com>
2021-06-15 15:46:13 -0400

    Added hook to scroll to top when route is changed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 import { Landing } from './components/landing/Landing'
 import { Home } from './components/home/Home'
+import ScrollToTop from './hooks/ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Switch>
         <Route exact path='/' component={Landing} />
         <Route path='/home' component={Home} />
